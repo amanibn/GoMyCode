65e3f326b3e41d9be22e19cd94d7fda06ad4b45c
amanibn <benahmedamani44@gmail.com>
2021-09-24 23:41:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Forms from './components/Forms';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Forms/>
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+  */}
     </div>
   );
 }
