2b90ad3deb620c48794a60ab4546adccc85d3544
amanibn <benahmedamani44@gmail.com>
2021-09-25 10:49:30 +0100

    My second commit

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,35 +1,41 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+import { Container } from "react-bootstrap";
 const Forms = () =>{
     return(
         <Container>
-  <Row>
-        <Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-</Row>
-</Container>
+        <div className="col-md-6">
+      <h4>Mon premier react form</h4>
+      <Form>
+      <Form.Group>
+          <Form.Label>Nom</Form.Label>
+          <Form.Control type="text" 
+                        placeholder="Nom" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" 
+                        placeholder="Email" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Âge</Form.Label>
+          <Form.Control type="number" placeholder="Âge" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Mot de passe</Form.Label>
+          <Form.Control type="password" placeholder="Mot de passe" />
+        </Form.Group>
+        <Form.Group>
+        <Button className="connexion" variant="primary" type="submit">
+           S'inscrire
+        </Button>
+        </Form.Group>
+      </Form>
+    </div>
+    </Container>
     )
 }
 export default Forms;
